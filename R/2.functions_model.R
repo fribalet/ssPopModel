64a08e4cd5a7082181f0bf86a72ff7433f9074af
Francois Ribalet <ribalet@uw.edu>
2019-05-02 08:39:54 -0700

    fined tuning

--- R/2.functions_model.R
+++ R/2.functions_model.R
@@ -1,8 +1,11 @@
+#########################
+### LOAD EXAMPLE DATA ###
+#########################
 library(R.matlab)
 results <- readMat("~/Documents/DATA/Codes/ssPopModel/inst/sosik2003/results.mat")
 df <- readMat("~/Documents/DATA/Codes/ssPopModel/inst/sosik2003/day733320data.mat")
 time <- seq(0,1+3600*24,by=3600)
-volbins <- (df$volbins)*220 #  fg 9instead of volume)
+volbins <- (df$volbins)*220 #  fg (instead of volume)
 V.hists <- df$Vhists
 	colnames(V.hists) <- time
 	row.names(V.hists) <- volbins
@@ -13,6 +16,21 @@ N.dist <- df$N.dist
 Edata <- df$Edata
 	Edata[,1] <- seq(range(time)[1], range(time)[2], length.out=nrow(Edata))
 
+dt <- resol/60
+	ime.interval <- median(diff(as.numeric(colnames(V.hists))))
+	ti <- as.numeric(colnames(V.hists))
+
+	# create Light data with 'dt' time interval.
+		seq <- NULL
+		for(i in 1:(length(ti)-1)){
+			s <- seq(ti[i], ti[i+1], length.out=1/dt)
+			seq <- c(seq, s)
+		}
+
+	ep <- data.frame(spline(Edata[,1], Edata[,2], xout=seq)) #interpolate E data according to dt resolution
+	Einterp <- ep$y
+	Einterp[Einterp < 0] <- 0
+
 
 	gmax <- 0.147
 	a <-1.239
@@ -20,7 +38,8 @@ Edata <- df$Edata
 	E_star <- 124.15
 	dmax <- 0.03
 
-
+	resol <- 10
+	hr <- 1
 
 
 
@@ -35,7 +54,7 @@ Edata <- df$Edata
 		########################
 		## INITIAL PARAMETERS ##
 		########################
-		t.nodiv <- 5 # # no division during the first X hours after dawn
+		t.nodiv <- 0 # no division during the first X hours after dawn
 		dt <- resol/60
 		j <- findInterval(2 * volbins[1], volbins)
 		m <- length(volbins) ## dimensions of the squared matrix
@@ -54,19 +73,21 @@ Edata <- df$Edata
 				# 2) rate of respiration constant (h-1) constant over the nighttime period (polysaccharide is drawn down linearly over the nighttime period)
 
 
-		resp <- 0*c*(1.35294 * y / dt+ 0.05018) # from Zavrel et al. 2019 eLife. Simplification: reg <- lm(seq(0.084, 0.199, length.out=10) ~ seq(0.025*dt, 0.11*dt, length.out=10))
+		resp <- c*(1.35294 * y + 0.05018) * dt# from Zavrel et al. 2019 eLife. Simplification: reg <- lm(seq(0.084, 0.199, length.out=10) ~ seq(0.025*dt, 0.11*dt, length.out=10))
 
-		#plot(y, type='l');lines(resp,col=2)
+		# par(mfrow=c(2,1))
+		# plot(y, type='l');lines(resp,col=2)
+		# plot(Einterp,y, type='p');points(Einterp, resp,col=2)
 		####################
 		## DELTA FUNCTION ## fraction of cells that divide between t and t + dt
 		####################
 		a <- 1 # in Hunter-Cevera et al. 2014
 		del <- dmax * (a*volbins)^b / (1 + (a*volbins)^b) # SOSIK et al. 2003
 
-		# NOTE: most values of volbinske need tp be < 1 for compatibility issue with the Delta function # based on HYNES et al. 2015
+		# NOTE: most values of volbins need to be < 1 for compatibility issue with the Delta function # based on HYNES et al. 2015
 
 
-		del[1:(j-1)] <- 0
+		# del[1:(j-1)] <- 0
 				if(hr <= t.nodiv){delta <- matrix(data=0, 1, m)
 					}else{delta <- matrix(del, 1, m)}
 
@@ -82,11 +103,16 @@ Edata <- df$Edata
 		################################
 		stasis_ind <- seq(1,m^2,by=m+1) # Diagonal stasis (0)
 		growth_ind <- seq(2,m^2,by=m+1) # Subdiagonal growth (-1)
-		resp_ind <- seq(((m)+1), m^2, by=m+1) # superdiagonal (+1)
+		resp_ind <- seq(m+1, m^2, by=m+1) # superdiagonal (+1)
 		div_ind <- seq((((j-1)*m)+1), m^2, by=m+1) # Superdiagonal division (j-1)
 
 		for(t in 1:(1/dt)){
+			#t <- 1
 			A <- matrix(data=0,nrow=m, ncol=m)
+
+			# Stasis (main diagonal)
+			A[stasis_ind] <- (1-delta)*(1-y[t+hr/dt])*(1-resp[t+hr/dt]) # the hr/dt part in the indexing is because each hour is broken up into dt segments for the irradiance spline
+			A[m,m] <- (1-delta[m])*(1-resp[t+hr/dt])
 
 			# Cell growth (subdiagonal)
 			A[growth_ind] <- y[t+hr/dt]*(1-delta[1:(m-1)])*(1-resp[t+hr/dt])
@@ -95,13 +121,8 @@ Edata <- df$Edata
 			A[1,1:(j-1)] <- A[1,1:(j-1)]  + 2 * delta[1:(j-1)] # Top row; Small phytoplanktoin (i=1,..., j-1) are less than twice as big as the smallest size class, and so newly divided are put in the smallest size class.
 			A[div_ind] <- 2 * delta[j:m] # The cell division terms for large (i > = j) phytoplankton
 
-			# Stasis (main diagonal)
-			A[stasis_ind] <- (1-delta)*(1-y[t+hr/dt])*(1-resp[t+hr/dt]) # the hr/dt part in the indexing is because each hour is broken up into dt segments for the irradiance spline
-			A[1,1] <- (1-y[t+hr/dt])*(1-resp[t+hr/dt])
-			A[m,m] <- (1-delta[m])*(1-resp[t+hr/dt])
-
 			# Respiration (superdiagonal)
-			A[1,2] <- resp[t+hr/dt]
+			A[1,2] <- A[1,2]  + resp[t+hr/dt]
 			A[resp_ind] <- resp[t+hr/dt]*(1-delta[-1])*(1-y[t+hr/dt])
 
 					if(t == 1){B <- A}else{B <- A %*% B}
@@ -116,15 +137,6 @@ Edata <- df$Edata
 ###############
 # This function calculates the sum of squares of the of the differences between the hourly observations and the model given the specified parameters
 # This function returns a column vector - called by "determine.opt.para" for the optimization.
-		gmax <- 0.14
-		a <-1.23
-		b <- 3.77
-		E_star <- 124
-		dmax <- 1
-		c <- 0.1
-		params <- data.frame(cbind(gmax, dmax, b, E_star, c))
-
-
 
 	.sigma.lsq <- function(params, Einterp, N.dist, V.hists, resol){
 
@@ -194,7 +206,7 @@ Edata <- df$Edata
 
 		f <- function(params) .sigma.lsq(params=params, Einterp=Einterp, N.dist=N.dist, V.hists=V.hists, resol=resol)
 
-		opt <- DEoptim(f, lower=c(1e-6,1e-6,1e-6,1,1e-6), upper=c(1,1,15,max(Einterp),1), control=DEoptim.control(itermax=1000, reltol=1e-4, trace=10, steptol=100, strategy=2, parallelType=0))
+		opt <- DEoptim(f, lower=c(1e-6,1e-6,1e-6,1,1e-6), upper=c(1,1,15,max(Einterp),1), control=DEoptim.control(itermax=1000, reltol=1e-2, trace=10, steptol=100, strategy=2, parallelType=0))
 
 		params <- opt$optim$bestmem
 		gmax <- params[1]
@@ -217,8 +229,9 @@ Edata <- df$Edata
 		volbins <- as.numeric(row.names(V.hists))
 
 		for(hr in res){
+
 					B <- .matrix.conct.fast(hr=hr-1, Einterp=Einterp, volbins=volbins, gmax=gmax, b=b, E_star=E_star,dmax=dmax, c=c, resol=resol)
-					Nproj[,hr+1] <- round(B %*% Nproj[,hr]) # calculate numbers of individuals
+					Nproj[,hr+1] <- B %*% Nproj[,hr] # calculate numbers of individuals
 					Vproj[,hr+1] <- B %*% Vproj[,hr] # calculate the projected size-frequency distribution
 					Vproj[,hr+1] <- Vproj[,hr+1]/sum(Vproj[,hr+1]) # normalize distribution
 					mu_N[,hr+1] <- log(sum(Nproj[,hr+1])/sum(Nproj[,hr]))/
@@ -227,10 +240,10 @@ Edata <- df$Edata
 		Nproj <- colSums(Nproj)
 		colnames(mu_N) <- colnames(Vproj)
 
-		##############################
+		#############################
 		## Growth rate calculation ##
-		##############################
-		d.mu_N <- 24*mean(mu_N, na.rm=T)
+		#############################
+		d.mu_N <- 24*mean(mu_N[-1], na.rm=T)
 		print(paste("daily growth rate=",round(d.mu_N,2)))
 
 		modelresults <- data.frame(cbind(gmax,dmax,b,E_star,c, resnorm), row.names=NULL)
@@ -241,8 +254,8 @@ Edata <- df$Edata
 }
 
 
-plot(mu_N[1,],col=3)
+plot(mu_N[1,],type='o')
 Vdiff <- V.hists - Vproj
-plot.size.distribution(Vdiff, type='l')
+plot.size.distribution(Vdiff, type='l',lwd=2)
 plot.size.distribution(V.hists, type='l')
 plot.size.distribution(Vproj, type='l')
