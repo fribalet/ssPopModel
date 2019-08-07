c02e79a900ec348f3fb1ba6ee0dc4365029cba86
Francois Ribalet <ribalet@uw.edu>
2019-08-07 15:40:40 -0700

    add smoothing option in plot.size.distribution

--- R/1.size_distribution.R
+++ R/1.size_distribution.R
@@ -1,17 +1,10 @@
-# db <- '~/Documents/DATA/MESO_SCOPE.db'
-# vct.dir <- '~/Documents/DATA/MESO_SCOPE_vct'
-# distribution <- size.distribution(db, vct.dir, binwidth=0.05, quantile=2.5, popname=NULL, channel='diam_mid')
-# plot(as.numeric(row.names(distribution))[-c(1:60)], distribution[-c(1:60),1], log='x'); points(min.volume , 0, col=2)
-# plot.size.distribution(distribution[-c(1:60),], mode='log', type='l')
-#   write.csv(distribution[-c(1:60),], '~/Desktop/MESO_SCOPE_PDF.csv',quote=F)
-# plot(vct.table[,'diam_mid'])
-
 ################################
 ### CREATE size distribution ###
 ################################
-size.distribution <- function(db, vct.dir,binwidth=0.05, quantile=c(2.5, 50,97.5),
-                              popname=c('prochloro','synecho','picoeuk',NULL),
-                              channel=c('diam_lwr', 'diam_mid', 'diam_upr')){
+size.distribution <- function(db, vct.dir,binwidth=0.05,
+                      quantile=c(2.5, 50,97.5),
+                      popname=c(NULL, 'prochloro','synecho','picoeuk','croco'),
+                      channel=c('diam_mid','vol_mid','Qc_mid')){
 
   QUANT <- as.numeric(quantile)
 
@@ -29,103 +22,117 @@ size.distribution <- function(db, vct.dir,binwidth=0.05, quantile=c(2.5, 50,97.5
   # Select on files that contains the population of interest
   if(!is.null(popname)) vct.table <- vct.table[vct.table$pop == popname,]
 
-  # cset bining of size distribution
-  lim <- range(vct.table[,channel]) # range of mean cell diameter
-  if(!is.null(popname)){ min.volume <- 0.5 * (4/3 * pi * (0.5*lim[1])^3) # half the volume of the smallest mean cell volumr
-                         max.volume <- 2 * (4/3 * pi * (0.5*lim[2])^3) # twice the volume of the largest mean cell volumr
-                       }else{
-                         min.volume <- (4/3 * pi * (0.5*lim[1])^3)
-                         max.volume <- (4/3 * pi * (0.5*lim[2])^3)
-                       }
-  breaks <- round(2^seq(log2(min.volume), log2(max.volume), by=binwidth),5)
+  # set bining of PDF
+    CHANNEL <- channel
+  if(any(channel == c('volume_lwr','volume_mid','volume_upr'))){
+    CHANNEL <- sub('volume','diam',channel)
+    }
+    # range of mean value
+    lim <- range(vct.table[,CHANNEL])
+    # 1/10 the smallest mean cell value
+    min <- lim[1] / 10
+    if(CHANNEL == channel) min <- 0.1 * 4/3 * pi * (0.5*lim[1])^3
+    # 10 x the largest mean cell value
+    max <- 10 * lim[2]
+    if(CHANNEL == channel) max <- 10 * 4/3 * pi * (0.5*lim[2])^3
+  breaks <- round(seq(min, max, by=binwidth),5)
+    if(log) breaks <- round(2^seq(log2(min), log2(max), by=binwidth),5)
 
   # Get Time from metadata
   sfl <- get.sfl.table(db)
 
-  # Get info to determine volume analyzed per sample
-  inst <- get.inst(db) # instrument serial Number
-  opp <- subset(get.opp.table(db), quantile == QUANT) # volume of virtual core for each sample
+  # Get volume analyzed per sample
+    # instrument serial Number
+    inst <- get.inst(db)
+    # volume of virtual core for each sample
+  opp <- subset(get.opp.table(db), quantile == QUANT)
 
 
   # Return list of unique files
-  #############################
-
   vct.list <- unique(vct.table$file)
 
-  i <- 1
-  distribution <- NULL
+  i <- 2
+  distribution <- data.frame(round(zoo::rollmean(breaks, 2),5))
+  names(distribution) <- channel
 
+
+  ##################################
+  ### create PDF for each sample ###
+  ##################################
   for(file.name in vct.list){
 
     message(round(100*i/length(vct.list)), "% completed \r", appendLF=FALSE)
 
     #retrieve time
-    time <- as.POSIXct(sfl[sfl$file == file.name, 'date'], format = "%FT%T", tz = "GMT")
+    time <- sfl[sfl$file == file.name, 'date']
 
-    #retrieve volume of virtual correct
-    fr <- 1000*flowrate(sfl[sfl$file == file.name, 'stream_pressure'], inst=inst)$flow_rate # flow rate (µL min-1)
-    acq.time <- sfl[sfl$file == file.name, 'file_duration']/60 # acquisition time (min)
-    opp.evt.ratio <- opp[opp$file == file.name,'opp_evt_ratio'] # opp/evt
-    vol <- opp.evt.ratio * fr * acq.time
+    # retrieve volume of virtual correct
+      # flow rate (mL min-1)
+      fr <- flowrate(sfl[sfl$file == file.name, 'stream_pressure'], inst=inst)$flow_rate
+      # (µL min-1)
+      fr <- fr * 1000
+      # acquisition time (min)
+      acq.time <- sfl[sfl$file == file.name, 'file_duration']/60
+      # opp/evt
+      opp.evt.ratio <- opp[opp$file == file.name,'opp_evt_ratio']
+    volume <- opp.evt.ratio * fr * acq.time
 
     #retrieve data
     vct <- get.vct.by.file(vct.dir, file.name, quantile=QUANT)
 
     if (!is.null(popname)) {
-      size <- vct[vct$pop == popname, channel]
+      dat <- vct[vct$pop == popname, CHANNEL]
     }else{
-      size <- vct[vct$pop != 'beads', channel]
+      dat <- vct[vct$pop != 'beads', CHANNEL]
     }
 
-    # calcualte Volume from diameter, assuming spherical shape
-    volume <- 4/3 * pi * (size/2)^3
+    if(CHANNEL == channel) dat <- 4/3 * pi * (dat/2)^3
 
     # Get particle count in each bin
-    dist <- data.frame(table(cut(size, breaks)))
+    dist <- data.frame(table(cut(dat, breaks)))
 
-    # Get particle concentration in each bin (cells µL-1)
-    PDF <- round(dist$Freq / vol, 5)
+    # Get particle concentration in each bin (10^-3 cells µL-1 or 10^3 cells L-1)
+    PDF <- round(1000 * dist$Freq / volume, 3)
 
     # add new data to the table
     distribution <- data.frame(cbind(distribution, PDF),check.names = FALSE)
-    colnames(distribution)[i] <- as.character(time)
-    rownames(distribution) <- round(rollmean(breaks, 2),5)
+    colnames(distribution)[i] <- time
 
     i <- i + 1
     flush.console()
   }
 
+  # remove rows outside actual size range (zeros), but include the two extremes
+  dim <- range(which(apply(distribution[,-c(1)], 1, sum) != 0))
+  distribution <- distribution[(dim[1]-1):(dim[2]+1),]
+
   return(distribution)
 
 }
 
-
 ##############################
 ### PLOT size distribution ###
 ##############################
-plot.size.distribution <- function(distribution, mode = c('log', 'lin'), ...){
+plot.size.distribution <- function(distribution, log=TRUE, smooth=0){
 
-    require(rgl)
-    jet.colors <- viridis::viridis
-    mode <- as.character(mode[1])
+    require(plotly)
+    require(oce)
 
-    param <- distribution
-    percentile <- cut(unlist(param), 100)
+    # smooth distribution
+    param <- data.frame(oce::matrixSmooth(as.matrix(distribution[,-c(1)]), pass=smooth))
+    param <- data.frame(cbind(distribution[,1], param))
+    colnames(param) <- colnames(distribution)
+    varname <- colnames(distribution)[1]
 
-    # in linear scale
-    if(mode =='lin'){
-        plot3d(rep(as.numeric(row.names(param)), dim(param)[2]),
-                rep(as.POSIXct(colnames(param), tz="GMT"), each=dim(param)[1]) ,
-                unlist(param),
-                col=jet.colors(100)[percentile])#, xlab="size class", ylab="time", zlab="Frequency",...)
-     }
+    abundance <- as.matrix(param)[,-c(1)]
+    time <- as.POSIXct(colnames(param), format = "%FT%T", tz="GMT")[-1]
+    names(param)[1] <- 'tbd'
 
+    p <- plotly::plot_ly(data=param, x= ~ time, y = ~ tbd, z = ~ abundance) %>%
+                    add_surface() %>%
+                    layout(scene = list(xaxis = list(autorange = "reversed", yaxis= list(title=paste(varname)))))
 
-    # in log scale
-    if(mode =='log'){
-        plot3d(log2(rep(as.numeric(row.names(param)), dim(param)[2])),
-                rep(as.POSIXct(colnames(param)), each=dim(param)[1]) ,
-                unlist(param),
-                col=jet.colors(100)[percentile],  xlab="size class", ylab="time", zlab="Frequency",...)
-    }
+    if(log) p <- p %>% plotly::layout(scene = list(yaxis = list(type = "log",title=paste(varname))))
+
+    return(p)
 }
